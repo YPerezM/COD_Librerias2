b26ab06648f73e5909941c2c0ea508965f449a36
Administrador <Administrador@LENOVO-842AF4C6>
2015-02-09 09:17:26 +0100

    COmmit inicial

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,20 @@
+
+
+package holamundo;
+
+import MiPaquete.Imprimir;
+
+/**
+ *
+ * @author Administrador
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO");
+        
+        Imprimir.saludo();
+        
+    }
+
+}
